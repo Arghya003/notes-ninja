d27da6d1cb598a41d524d714a32921baa3211eb9
Arghya003 <arghyaguha105@gmail.com>
2024-05-08 12:45:08 +0530

    Home Page UI created

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,42 @@
 import Image from "next/image";
-import { ThemeToggle } from "./components/Themetoggle";
 
-export default function Home() {
-  return (
-   <div>
-   
+import { Button } from "@/components/ui/button";
+
+
+export default async function Home() {
     
-   </div>
+  return (
+    <section className="flex items-center justify-center bg-background h-[90vh]">
+      <div className=" relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
+        <div className="max-w-3xl mx-auto text-center">
+          <div>
+            <span className="w-auto px-6 py-3 rounded-full bg-secondary">
+              <span className="text-md font-medium text-primary ">
+                {" "}
+                Sort your notes easily
+              </span>
+            </span>
+
+            <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
+              Create Notes with Ease
+            </h1>
+            <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
+              Note Ninja is your ultimate ally in note-taking, revolutionizing
+              how you capture information. With its intuitive interface and
+              advanced features, this app streamlines the process, letting you
+              focus on learning. Enhance your study experience with Note Ninja.
+              Master your notes effortlessly
+            </p>
+          </div>
+          <div className="flex justify-center max-w-sm mx-auto mt-10">
+        
+              <Button size="lg" className="w-full">
+                Sign Up for free
+              </Button>
+            
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
