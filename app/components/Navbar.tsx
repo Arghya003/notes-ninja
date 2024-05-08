30af459aec50006658c91633c0f6aa546203521a
Arghya003 <arghyaguha105@gmail.com>
2024-05-08 12:14:30 +0530

    Autheticated with Kinde

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,26 +1,37 @@
 import  Link  from "next/link";
 import { ThemeToggle } from "./Themetoggle";
 import { Button } from "@/components/ui/button";
+import {RegisterLink, LoginLink,LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar(){
-    return(
-        <nav className="border-b bg-background h-[10vh] flex items-center">
-            <div className="container flex items-center justify-between">
-                <Link href='/'>
-                    <h1 className="font-bold text-3xl">NoteNinja</h1>
-                </Link>
-        <div className="flex items-center gap-x-5">
+export async function Navbar(){
+    const{isAuthenticated}=getKindeServerSession();
 
-            <ThemeToggle/>
-            <div className="flex items-end gap-x-5">
-                <Button>SignIn</Button>
-                <Button variant={'secondary'}>SignUp</Button>
+    return (
+      <nav className="border-b bg-background h-[10vh] flex items-center">
+        <div className="container flex items-center justify-between">
+          <Link href="/">
+            <h1 className="font-bold text-3xl">NoteNinja</h1>
+          </Link>
+          <div className="flex items-center gap-x-5">
+            <ThemeToggle />
+            {(await isAuthenticated()) ? (
+              <LogoutLink>
+                <Button>Logout</Button>
+              </LogoutLink>
+            ) : (
+              <div className="flex items-center gap-x-5">
+                <LoginLink>
+                  <Button>Sign In</Button>
+                </LoginLink>
 
-            </div>
-
+                <RegisterLink>
+                  <Button variant="secondary">Sign Up</Button>
+                </RegisterLink>
+              </div>
+            )}
+          </div>
         </div>
-            </div>
-
-        </nav>
-    )
+      </nav>
+    );
 }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashBoardPage(){
+    return (
+      <div>
+        <h1>Hello from the DashBoard</h1>
+      </div>
+    );
+ 
+}
