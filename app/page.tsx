943c0d9a0cd419f89ac50602fb77a8bfc9b1cd4e
Arghya003 <arghyaguha105@gmail.com>
2024-05-09 10:32:34 +0530

    made the dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 import Image from "next/image";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import { Button } from "@/components/ui/button";
 
@@ -7,6 +13,7 @@ export default async function Home() {
     
   return (
     <section className="flex items-center justify-center bg-background h-[90vh]">
+      
       <div className=" relative items-center w-full px-5 py-12 mx-auto lg:px-16 max-w-7xl md:px-12">
         <div className="max-w-3xl mx-auto text-center">
           <div>
@@ -29,11 +36,11 @@ export default async function Home() {
             </p>
           </div>
           <div className="flex justify-center max-w-sm mx-auto mt-10">
-        
+            <RegisterLink>
               <Button size="lg" className="w-full">
                 Sign Up for free
               </Button>
-            
+            </RegisterLink>
           </div>
         </div>
       </div>
