f5d8e0fca5f0917b794ecf040f5a1d5a423f07eb
Arghya003 <arghyaguha105@gmail.com>
2024-05-16 18:56:01 +0530

    created a basic webhook for client communication

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,12 @@
+import { headers } from "next/headers";
+import Stripe from "stripe";
+
+export async function POST(req:Request){
+    const body=await req.text();
+
+    const signature=headers().get('Stripe-Signature')
+    
+    let event:Stripe.Event
+
+    
+}
